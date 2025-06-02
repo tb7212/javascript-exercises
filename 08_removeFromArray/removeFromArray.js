1c1e28501eae1b444a4776dc8818ce26a83d9daf
Tasdiq Bashar <tasdiqbashar@gmail.com>
2025-06-02 17:31:13 -0400

    Loop and arrays exercises complete

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = Array.from(arguments).slice(1);
+    return arr.filter(arg => !(args.includes(arg)));
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0){
+        return "ERROR";
+    }
+    if(!(Number.isInteger(start))||!(Number.isInteger(end))){
+        return "ERROR";
+    }
+    if(end < start){
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+    let total = 0;
+    for(i = start; i <= end; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
