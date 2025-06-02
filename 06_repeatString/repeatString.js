a26563bf13c2b16f899e5b126b7e03fb6b155e0c
Tasdiq Bashar <tasdiqbashar@gmail.com>
2025-06-02 16:42:15 -0400

    Completed up to exercise 7

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    let newString = '';
+    for(i = 0; i < num; i++){
+        newString = newString.concat(str);
+    }
+    return newString;
 };
 
 // Do not edit below this line
