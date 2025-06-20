7cf06c3caf2b40afe547f997cd463c64852ed808
Tasdiq Bashar <tasdiqbashar@gmail.com>
2025-06-20 15:45:24 -0400

    All exercises completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let forward = 0;
+    let reverse = str.length - 1;
+    const punctuation = ' .,:;!?\'"()-[]{}/\\@#$%^&*_+|=~`<>'
+    while (forward < (str.length / 2)){
+        while (punctuation.includes(str[forward])){
+            forward++;
+        }
+        while (punctuation.includes(str[reverse])){
+            reverse--;
+        }
+        if (str[forward] !== str[reverse]){
+            return false;
+        }
+        forward++;
+        reverse--;
+    }
+    return true;
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((personA, personB) => {
+        const currentYear = new Date().getFullYear();
+        personA.yearOfDeath = personA.yearOfDeath ? personA.yearOfDeath : currentYear;
+        personB.yearOfDeath = personB.yearOfDeath ? personB.yearOfDeath : currentYear;
+        let personAAge = personA.yearOfDeath - personA.yearOfBirth;
+        let personBAge = personB.yearOfDeath - personB.yearOfBirth;
+        return personBAge - personAAge;
+    })
+    return people[0];
 };
 
 // Do not edit below this line
